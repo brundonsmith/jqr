a394b83905e8b0f0d0feadf372ff6701c7e8aa4f
Brandon Smith <mail@brandonsmith.ninja>
2020-10-09 16:58:45 -0500

    Rewrote JSON parser not to have a separate tokenize phase, eliminating a bunch of value-copying and dramatically improving performance

--- src/json_parser.rs
+++ src/json_parser.rs
@@ -4,229 +4,115 @@ use std::{collections::HashMap};
 use crate::model::{JSONValue, StrOrString};
 
 
+
+#[derive(Debug,PartialEq)]
+pub struct ParseError {
+    pub msg: String,
+    pub index: usize,
+}
+
+const TRUE_TOKEN: &str = "true";
+const TRUE_TOKEN_LEN: usize = TRUE_TOKEN.len();
+const FALSE_TOKEN: &str = "false";
+const FALSE_TOKEN_LEN: usize = FALSE_TOKEN.len();
+const NULL_TOKEN: &str = "null";
+const NULL_TOKEN_LEN: usize = NULL_TOKEN.len();
+
+
+// parsing
 pub fn parse<'a>(code: &'a str) -> impl Iterator<Item=Result<JSONValue<'a>,ParseError>> {
-    let tokens: Vec<Token> = tokenize(code).collect();
     let mut index = 0;
 
     std::iter::from_fn(move || {
-        if index < tokens.len() {
-            Some(expression(&tokens, &mut index))
+        consume_whitespace(code, &mut index);
+        if index < code.len() {
+            Some(expression(code, &mut index))
         } else {
             None
         }
     })
 }
 
-#[derive(Debug,PartialEq)]
-pub struct ParseError<'a> {
-    pub msg: String,
-    pub token: Token<'a>,
-}
+fn expression<'a>(code: &'a str, index: &mut usize) -> Result<JSONValue<'a>, ParseError> {
+    let ch = code[*index..].chars().next();
 
-
-
-// tokenizing 
-#[derive(Debug,Clone,PartialEq)]
-pub struct Token<'a> {
-    pub index: usize,
-    pub lexeme: JSONLexeme<'a>,
-}
-
-#[derive(Debug,Clone,PartialEq)]
-pub enum JSONLexeme<'a> {
-    Special(&'static char),
-    String(&'a str),
-    AllocatedString(String),
-    Integer(i32),
-    Float(f32),
-    Boolean(bool),
-    Null
-}
-
-fn tokenize<'a>(code: &'a str) -> impl Iterator<Item = Token<'a>> {
-    let mut skip_to: Option<usize> = None;
-
-    code.char_indices().filter_map(move |(index, ch)| {
-        if skip_to
-            .map(|destination| destination > index)
-            .unwrap_or(false)
-        {
-            return None;
-        } else {
-            skip_to = None;
-        }
-
-        // whitespace
-        if ch.is_whitespace() {
-            return None;
-        }
-
-        // strings
-        if ch == '"' {
-            let mut escape_next = false;
-            let mut allocated_string: Option<String> = None;
-            let contents_start = index + 1;
-            let mut end = contents_start;
-            for (i, c) in code[index + 1..].char_indices() {
-                let current_index = contents_start + i;
-                if c == '\\' && !escape_next {
-                    escape_next = true;
-                    if allocated_string.is_none() {
-                        allocated_string = Some(String::from(&code[contents_start..current_index]));
-                    }
-                } else {
-                    if c == '"' && !escape_next {
-                        end = contents_start + i;
-                        skip_to = Some(current_index + 1);
-                        break;
-                    }
-                    
-                    match &mut allocated_string {
-                        Some(s) => s.push(c),
-                        None => {},
-                    };
-
-                    escape_next = false;
-                }
-            }
-
-            return Some(Token { 
-                index,
-                lexeme: match allocated_string {
-                    Some(s) => JSONLexeme::AllocatedString(s),
-                    None => JSONLexeme::String(&code[index + 1..end]),
-                }
-            });
-        }
-
-        // special tokens
-        for special in &SPECIAL_TOKENS {
-            if code[index..].chars().nth(0).as_ref() == Some(special) {
-                skip_to = Some(index + 1);
-                return Some(Token { index, lexeme: JSONLexeme::Special(special) });
-            }
-        }
-
-        if match_front(&code[index..], TRUE_TOKEN) {
-            skip_to = Some(index + TRUE_TOKE_LEN);
-            return Some(Token { index, lexeme: JSONLexeme::Boolean(true) });
-        }
-
-        if match_front(&code[index..], FALSE_TOKEN) {
-            skip_to = Some(index + FALSE_TOKE_LEN);
-            return Some(Token { index, lexeme: JSONLexeme::Boolean(false) });
-        }
-
-        if match_front(&code[index..], NULL_TOKEN) {
-            skip_to = Some(index + NULL_TOKE_LEN);
-            return Some(Token { index, lexeme: JSONLexeme::Null });
-        }
-
-        // numbers
-        if ch.is_numeric() {
-            let front_end = index + code[index..].char_indices()
-                .take_while(|(_, c)| c.is_numeric())
-                .last()
-                .map(|(index, ch)| index + ch.len_utf8())
-                .unwrap_or(0);
-
-            if code[front_end..].chars().next() == Some('.') {
-                let back_end_start = front_end + 1;
-                let back_end = back_end_start + code[back_end_start..].char_indices()
-                    .take_while(|(_, c)| c.is_numeric())
-                    .last()
-                    .map(|(index, ch)| index + ch.len_utf8())
-                    .unwrap_or(0);
-
-                skip_to = Some(back_end);
-                return Some(Token {
-                    index,
-                    lexeme: JSONLexeme::Float(code[index..back_end].parse().unwrap())
-                });
-            } else {
-                skip_to = Some(front_end);
-                return Some(Token {
-                    index,
-                    lexeme: JSONLexeme::Integer(code[index..front_end].parse().unwrap())
-                });
-            }
-        }
-
-        return None;
-    })
-}
-
-fn match_front(code: &str, segment: &str) -> bool {
-    segment.chars().zip(code.chars()).all(|(a, b)| a == b)
-}
-
-const SPECIAL_TOKENS: [char; 6] = ['{', '}', '[', ']', ',', ':'];
-const TRUE_TOKEN: &str = "true";
-const TRUE_TOKE_LEN: usize = TRUE_TOKEN.len();
-const FALSE_TOKEN: &str = "false";
-const FALSE_TOKE_LEN: usize = FALSE_TOKEN.len();
-const NULL_TOKEN: &str = "null";
-const NULL_TOKE_LEN: usize = NULL_TOKEN.len();
-
-
-// parsing
-fn expression<'a>(tokens: &Vec<Token<'a>>, index: &mut usize) -> Result<JSONValue<'a>, ParseError<'a>> {
-    let next = &tokens[*index];
-    *index += 1;
-
-    match &next.lexeme {
-        JSONLexeme::String(s) => Ok(JSONValue::String(s)),
-        JSONLexeme::AllocatedString(s) => Ok(JSONValue::AllocatedString(s.clone())),
-        JSONLexeme::Integer(n) => Ok(JSONValue::Integer(*n)),
-        JSONLexeme::Float(n) => Ok(JSONValue::Float(*n)),
-        JSONLexeme::Boolean(b) => Ok(JSONValue::Boolean(*b)),
-        JSONLexeme::Null => Ok(JSONValue::Null),
-        JSONLexeme::Special('{') => object_body(tokens, index),
-        JSONLexeme::Special('[') => array_body(tokens, index),
-        JSONLexeme::Special(t) => Err(ParseError { 
-            msg: format!("Unexpected token '{}'", t), 
-            token: next.clone() 
-        }),
+    if ch == Some('"') {
+        string(code, index)
+    } else if ch == Some('{') {
+        object(code, index)
+    } else if ch == Some('[') {
+        array(code, index)
+    } else if ch.map(|c| c.is_numeric()).unwrap_or(false) {
+        number(code, index)
+    } else if match_front(&code[*index..], TRUE_TOKEN) {
+        *index += TRUE_TOKEN_LEN;
+        Ok(JSONValue::Boolean(true))
+    } else if match_front(&code[*index..], FALSE_TOKEN) {
+        *index += FALSE_TOKEN_LEN;
+        Ok(JSONValue::Boolean(false))
+    } else if match_front(&code[*index..], NULL_TOKEN) {
+        *index += NULL_TOKEN_LEN;
+        Ok(JSONValue::Null)
+    } else {
+        Err(ParseError {
+            msg: String::from("Expected JSON value"),
+            index: *index
+        })
     }
 }
 
-fn object_body<'a>(tokens: &Vec<Token<'a>>, index: &mut usize) -> Result<JSONValue<'a>, ParseError<'a>> {
-    if try_eat(tokens, index, &'}').is_ok() {
+fn object<'a>(code: &'a str, index: &mut usize) -> Result<JSONValue<'a>, ParseError> {
+    try_eat(code, index, &'{')?;
+
+    consume_whitespace(code, index);
+    if try_eat(code, index, &'}').is_ok() {
         return Ok(JSONValue::Object(HashMap::new()));
     }
 
     let mut contents = HashMap::new();
 
-    let key = expression(tokens, index);
+    consume_whitespace(code, index);
+    let key = expression(code, index);
 
-    if let Some(prop) = consume_str_or_string(key) {
-        try_eat(tokens, index, &':')?;
-        let value = expression(tokens, index)?;
+    consume_whitespace(code, index);
+    if let Some(prop) = as_str_or_string(key) {
+        consume_whitespace(code, index);
+        try_eat(code, index, &':')?;
+        consume_whitespace(code, index);
+        let value = expression(code, index)?;
 
         contents.insert(prop, value);
 
-        while try_eat(tokens, index, &',').is_ok() {
-            let key = expression(tokens, index);
+        consume_whitespace(code, index);
+        while try_eat(code, index, &',').is_ok() {
+            consume_whitespace(code, index);
+            let key = expression(code, index);
 
-            if let Some(prop) = consume_str_or_string(key) {
-                try_eat(tokens, index, &':')?;
-                let value = expression(tokens, index)?;
+            consume_whitespace(code, index);
+            if let Some(prop) = as_str_or_string(key) {
+                consume_whitespace(code, index);
+                try_eat(code, index, &':')?;
+                consume_whitespace(code, index);
+                let value = expression(code, index)?;
 
                 contents.insert(prop, value);
             } else {
                 return Err(ParseError {
                     msg: String::from("Expected object property after ','"),
-                    token: tokens[*index].clone(),
+                    index: *index,
                 });
             }
+
+            consume_whitespace(code, index);
         }
     }
-    try_eat(tokens, index, &'}')?;
+    consume_whitespace(code, index);
+    try_eat(code, index, &'}')?;
     
     return Ok(JSONValue::Object(contents));
 }
 
-fn consume_str_or_string<'a>(next: Result<JSONValue<'a>, ParseError>) -> Option<StrOrString<'a>> {
+fn as_str_or_string<'a>(next: Result<JSONValue<'a>, ParseError>) -> Option<StrOrString<'a>> {
     if let Ok(JSONValue::String(prop)) = next {
         return Some(StrOrString::Str(prop));
     } else if let Ok(JSONValue::AllocatedString(prop)) = next {
@@ -236,55 +122,122 @@ fn consume_str_or_string<'a>(next: Result<JSONValue<'a>, ParseError>) -> Option<
     }
 }
 
-fn array_body<'a>(tokens: &Vec<Token<'a>>, index: &mut usize) -> Result<JSONValue<'a>, ParseError<'a>> {
-    if try_eat(tokens, index, &']').is_ok() {
+fn array<'a>(code: &'a str, index: &mut usize) -> Result<JSONValue<'a>, ParseError> {
+    try_eat(code, index, &'[')?;
+
+    consume_whitespace(code, index);
+    if try_eat(code, index, &']').is_ok() {
         return Ok(JSONValue::Array(vec![]));
     }
 
     let mut contents = Vec::new();
 
-    if let Ok(value) = expression(tokens, index) {
+    consume_whitespace(code, index);
+    if let Ok(value) = expression(code, index) {
         contents.push(value);
 
-        while try_eat(tokens, index, &',').is_ok() {
-            let value = expression(tokens, index)?;
+        consume_whitespace(code, index);
+        while try_eat(code, index, &',').is_ok() {
+            consume_whitespace(code, index);
+            let value = expression(code, index)?;
             contents.push(value);
         }
     }
-    try_eat(tokens, index, &']')?;
+    consume_whitespace(code, index);
+    try_eat(code, index, &']')?;
 
     return Ok(JSONValue::Array(contents));
 }
 
-fn property_name<'a>(tokens: &Vec<Token<'a>>, index: &mut usize) -> Result<&'a str, ParseError<'a>> {
-    if let JSONLexeme::String(prop) = tokens[*index].lexeme {
-        *index += 1;
-        return Ok(prop);
+fn string<'a>(code: &'a str, index: &mut usize) -> Result<JSONValue<'a>, ParseError> {
+    let mut escape_next = false;
+    let mut allocated_string: Option<String> = None;
+    let contents_start = *index + 1;
+    let mut end = contents_start;
+    for (i, c) in code[*index + 1..].char_indices() {
+        let current_index = contents_start + i;
+        if c == '\\' && !escape_next {
+            escape_next = true;
+            if allocated_string.is_none() {
+                allocated_string = Some(String::from(&code[contents_start..current_index]));
+            }
+        } else {
+            if c == '"' && !escape_next {
+                end = contents_start + i;
+                *index = current_index + 1;
+                break;
+            }
+            
+            match &mut allocated_string {
+                Some(s) => s.push(c),
+                None => {},
+            };
+
+            escape_next = false;
+        }
+    }
+
+    Ok(match allocated_string {
+        Some(s) => JSONValue::AllocatedString(s),
+        None => JSONValue::String(&code[contents_start..end]),
+    })
+}
+
+fn number<'a>(code: &'a str, index: &mut usize) -> Result<JSONValue<'a>, ParseError> {
+    let start = *index;
+    let front_end = *index + code[*index..].char_indices()
+        .take_while(|(_, c)| c.is_numeric())
+        .last()
+        .map(|(index, ch)| index + ch.len_utf8())
+        .unwrap_or(0);
+
+    if code[front_end..].chars().next() == Some('.') {
+        let back_end_start = front_end + 1;
+        let back_end = back_end_start + code[back_end_start..].char_indices()
+            .take_while(|(_, c)| c.is_numeric())
+            .last()
+            .map(|(index, ch)| index + ch.len_utf8())
+            .unwrap_or(0);
+
+        *index = back_end;
+        return Ok(JSONValue::Float(code[start..back_end].parse().unwrap()));
     } else {
-        return Err(ParseError { 
-            msg: format!("Expected object property name"), 
-            token: tokens[*index].clone() 
-        });
+        *index = front_end;
+        return Ok(JSONValue::Integer(code[start..front_end].parse().unwrap()));
     }
 }
 
-fn try_eat<'a>(tokens: &Vec<Token<'a>>, index: &mut usize, expected: &char) -> Result<(), ParseError<'a>> {
-    if let JSONLexeme::Special(found) = tokens[*index].lexeme {
-        if found == expected {
-            *index += 1;
+fn consume_whitespace<'a>(code: &'a str, index: &mut usize) {
+    if let Some(length) = code[*index..]
+            .char_indices()
+            .take_while(|(_, ch)| ch.is_whitespace())
+            .last()
+            .map(|(i, ch)| i + ch.len_utf8()) {
+        *index += length;
+    }
+}
+
+fn try_eat<'a>(code: &'a str, index: &mut usize, expected: &char) -> Result<(), ParseError> {
+    if let Some(found) = code[*index..].chars().next() {
+        if &found == expected {
+            *index += expected.len_utf8();
             Ok(())
         } else {
             return Err(ParseError { 
                 msg: format!("Expected '{}', found '{}'", expected, found), 
-                token: tokens[*index].clone() 
+                index: *index
             });
         }
     } else {
         return Err(ParseError { 
             msg: format!("Expected '{}'", expected), 
-            token: tokens[*index].clone() 
+            index: *index
         });
     }
+}
+
+fn match_front(code: &str, segment: &str) -> bool {
+    code.len() >= segment.len() && segment.chars().zip(code.chars()).all(|(a, b)| a == b)
 }
 
 #[cfg(test)]
