d71182a3aa2153e4e5f22475f2bae9964b87ee14
Brandon Smith <mail@brandons.me>
2021-07-09 22:21:18 +0000

    More tweaks to benchmark output

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -2,8 +2,10 @@
 
 title () {
   echo ""
+  echo "------------------------------------------------------------"
   echo "$1"
   echo "$2"
+  echo ""
 }
 
 title "Running benchmark tests..."
@@ -17,81 +19,81 @@ test () {
 }
 
 title "### Pipe a json to stdin" "(N/A)"
-echo "-- jq --"
+echo "[ jq                                ]"
 test cat benchmarks/big.json | jq '.' > /dev/null
-echo "-- query-json --"
+echo "[ query-json                        ]"
 test cat benchmarks/big.json | query-json --kind=inline '.' > /dev/null
-echo "-- jqr (regular, no-free, streaming) --"
+echo "[ jqr (regular, no-free, streaming) ]"
 test cat benchmarks/big.json | jqr '.' > /dev/null
 test cat benchmarks/big.json | jqr '.' > /dev/null --no-free
 test cat benchmarks/big.json | jqr '.' > /dev/null --stream
 
 title "### Select an attribute on a small (4kb) JSON file" ".first.id"
-echo "-- jq --"
+echo "[ jq                                ]"
 test jq '.first.id' benchmarks/small.json > /dev/null
-echo "-- query-json --"
+echo "[ query-json                        ]"
 test query-json '.first.id' benchmarks/small.json > /dev/null
-echo "-- jqr (regular, no-free, streaming) --"
+echo "[ jqr (regular, no-free, streaming) ]"
 test jqr '.first.id' benchmarks/small.json > /dev/null
 test jqr '.first.id' benchmarks/small.json > /dev/null --no-free
 test jqr '.first.id' benchmarks/small.json > /dev/null --stream
 
 title "### Select an attribute on a medium (132k) JSON file" "."
-echo "-- jq --"
+echo "[ jq                                ]"
 test jq '.' benchmarks/medium.json > /dev/null
-echo "-- query-json --"
+echo "[ query-json                        ]"
 test query-json '.' benchmarks/medium.json > /dev/null
-echo "-- jqr (regular, no-free, streaming) --"
+echo "[ jqr (regular, no-free, streaming) ]"
 test jqr '.' benchmarks/medium.json > /dev/null
 test jqr '.' benchmarks/medium.json > /dev/null --no-free
 test jqr '.' benchmarks/medium.json > /dev/null --stream
 
 title "### Select an attribute on a big JSON (604k) file" "map(.)"
-echo "-- jq --"
+echo "[ jq                                ]"
 test jq 'map(.)' benchmarks/big.json > /dev/null
-echo "-- query-json --"
+echo "[ query-json                        ]"
 test query-json 'map(.)' benchmarks/big.json > /dev/null
-echo "-- jqr (regular, no-free, streaming) --"
+echo "[ jqr (regular, no-free, streaming) ]"
 test jqr 'map(.)' benchmarks/big.json > /dev/null
 test jqr 'map(.)' benchmarks/big.json > /dev/null --no-free
 test jqr 'map(.)' benchmarks/big.json > /dev/null --stream
 
 title "### Simple operation on a small (4kb) JSON file" ".second.store.books | map(.price + 10)"
-echo "-- jq --"
+echo "[ jq                                ]"
 test jq '.second.store.books | map(.price + 10)' benchmarks/small.json > /dev/null
-echo "-- query-json --"
+echo "[ query-json                        ]"
 test query-json '.second.store.books | map(.price + 10)' benchmarks/small.json > /dev/null
-echo "-- jqr (regular, no-free, streaming) --"
+echo "[ jqr (regular, no-free, streaming) ]"
 test jqr '.second.store.books | map(.price + 10)' benchmarks/small.json > /dev/null
 test jqr '.second.store.books | map(.price + 10)' benchmarks/small.json > /dev/null --no-free
 test jqr '.second.store.books | map(.price + 10)' benchmarks/small.json > /dev/null --stream
 
 title "### Simple operation on a medium (132k) JSON file" "map(.time)"
-echo "-- jq --"
+echo "[ jq                                ]"
 test jq 'map(.time)' benchmarks/medium.json > /dev/null
-echo "-- query-json --"
+echo "[ query-json                        ]"
 test query-json 'map(.time)' benchmarks/medium.json > /dev/null
-echo "-- jqr (regular, no-free, streaming) --"
+echo "[ jqr (regular, no-free, streaming) ]"
 test jqr 'map(.time)' benchmarks/medium.json > /dev/null
 test jqr 'map(.time)' benchmarks/medium.json > /dev/null --no-free
 test jqr 'map(.time)' benchmarks/medium.json > /dev/null --stream
 
 title "### Simple operation an attribute on a big JSON (604k) file" "map(select(.base.Attack > 100)) | map(.name.english)"
-echo "-- jq --"
+echo "[ jq                                ]"
 test jq 'map(select(.base.Attack > 100)) | map(.name.english)' benchmarks/big.json > /dev/null
-echo "-- query-json --"
+echo "[ query-json                        ]"
 test query-json 'filter(.base."Attack" > 100) | map(.name.english)' benchmarks/big.json > /dev/null
-echo "-- jqr (regular, no-free, streaming) --"
+echo "[ jqr (regular, no-free, streaming) ]"
 test jqr 'map(select(.base.Attack > 100)) | map(.name.english)' benchmarks/big.json > /dev/null
 test jqr 'map(select(.base.Attack > 100)) | map(.name.english)' benchmarks/big.json > /dev/null --no-free
 test jqr 'map(select(.base.Attack > 100)) | map(.name.english)' benchmarks/big.json > /dev/null --stream
 
 title "### Simple operation an attribute on a huge JSON (110M) file" "keys"
-echo "-- jq --"
+echo "[ jq                                ]"
 test jq 'keys' benchmarks/huge.json > /dev/null
-echo "-- query-json --"
+echo "[ query-json                        ]"
 test query-json 'keys' benchmarks/huge.json > /dev/null
-echo "-- jqr (regular, no-free, streaming) --"
+echo "[ jqr (regular, no-free, streaming) ]"
 test jqr 'keys' benchmarks/huge.json > /dev/null
 test jqr 'keys' benchmarks/huge.json > /dev/null --no-free
 test jqr 'keys' benchmarks/huge.json > /dev/null --stream
